87a938f06600b89df36c79fe8d80bb957b5b9bf5
Rebecca <135012814+rebeccaorso@users.noreply.github.com>
2024-05-03 10:57:27 +0200

    Delete factoryMethod directory

--- factoryMethod/BurgerPolloRistorante.java
+++
@@ -1,8 +0,0 @@
-package factoryMethod;
-
-public class BurgerPolloRistorante extends Ristorante {
-    @Override
-    protected Burger creaBurger(String tipo) {
-        return new PolloBurger();
-    }
-}
