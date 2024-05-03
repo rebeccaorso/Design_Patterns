97f46995b722b7842f2fe67f4f456921d5aa09a6
Rebecca <135012814+rebeccaorso@users.noreply.github.com>
2024-05-03 11:24:56 +0200

    Delete mediatorPattern directory

--- mediatorPattern/DialogMediator.java
+++
@@ -1,7 +0,0 @@
-package mediatorPattern;
-
-//Interfaccia del mediatore che fornisce il metodo notify per la comunicazione dei componenti.
-
-public interface DialogMediator {
-    void notify(Component sender, String event);
-}
