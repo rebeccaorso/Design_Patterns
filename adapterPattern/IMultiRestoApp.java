78645e8569e1c6f2e450d7929591294864c44fe9
Rebecca <135012814+rebeccaorso@users.noreply.github.com>
2024-05-03 12:02:55 +0200

    Delete adapterPattern directory

--- adapterPattern/IMultiRestoApp.java
+++
@@ -1,7 +0,0 @@
-package adapterPattern;
-
-//Definisce il contratto per le applicazioni che gestiscono la visualizzazione dei dati. Questo contratto
-// impone il metodo displayData che accetta dati in formato stringa (XML in questo caso).
-public interface IMultiRestoApp {
-    void displayData(String xmlData);
-}
