a88a32a47aa888088afaaf96d5945f8a1691fb57
Rebecca <135012814+rebeccaorso@users.noreply.github.com>
2024-05-03 12:13:12 +0200

    Delete facadePattern directory

--- facadePattern/NotificationService.java
+++
@@ -1,8 +0,0 @@
-package facadePattern;
-
-//Gestisce l'invio di notifiche agli utenti, come le email di conferma dopo una transazione.
-public class NotificationService {
-    public void sendConfirmationEmail(User user, Transaction transaction){
-        System.out.println("Confirmation email sent to " + user.getEmail());
-    }
-}
