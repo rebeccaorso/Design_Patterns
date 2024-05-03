acc19c841576d4aa9df84cc4200fbd16b0ea4afc
Rebecca <135012814+rebeccaorso@users.noreply.github.com>
2024-05-03 12:21:43 +0200

    Delete proxyPattern directory

--- proxyPattern/VideoDownloader.java
+++
@@ -1,6 +0,0 @@
-package proxyPattern;
-
-//Definisce le operazioni comuni che il proxy e il servizio reale devono implementare.
-public interface VideoDownloader {
-    Video downloadVideo(String videoId);
-}
