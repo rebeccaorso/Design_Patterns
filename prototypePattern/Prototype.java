f1fbc14254ae58a6919e91a80529234a81c631aa
Rebecca <135012814+rebeccaorso@users.noreply.github.com>
2024-05-03 11:11:42 +0200

    Delete prototypePattern directory

--- prototypePattern/Prototype.java
+++
@@ -1,7 +0,0 @@
-package prototypePattern;
-
-public interface Prototype {
-    //Definisce l'interfaccia per clonare se stessa. Questa interfaccia contiene tipicamente
-    // un solo metodo clone() che è utilizzato per creare una copia dell'oggetto.
-    Prototype clone();
-}
